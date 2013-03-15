0e9460c258c88e6443a6e8cba287f958d8aa3834
Tobias Kopelke <nox@demoncode.de>
2013-03-15 17:49:33 +0100

    Added images from lorempixel

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,11 +2,15 @@
 
 /* Controllers */
 
-var names_male    = ["Andrew Beckel","Mauricio Esterly","Gale Tarry","Winfred Dressel","Clement Monroe","Raymon Symons","Michale Deblois","Irvin Sturgis","Chi Meltzer","Bobby Martucci","Trey Schuessler","Toney Hysell","Tom Croley","David Pia","Lavern Lederman","Carter Lapp","Leopoldo Debelak","Spencer Whiteman","Everette Truax","Daryl Clouse","Jules Strothers","Shannon Siqueiros","Donn Nale","Gerard Stake","Kurt Plascencia","Byron Mignone","Sherman Horner","Scott Musick","Aubrey Doster","Dominique Goncalves","Jess Darr","Ben Bruemmer","Anthony Parks","Hugo Statton","Herman Carson","Adolph Silvas","Reggie Kunzman","Alfonso Dix","Howard Olmo","Earnest Sedlock","Phil Araya","Mervin Oshiro","Kerry Dugger","Wilber Rasberry","Samuel Walthall","Malcolm Mcmillan","Danial Franson","Cliff Lemarr","Sal Schewe","Daniel Sligh"]
-  , names_female  = ["Arie Pates","Ellena Crossett","Annmarie Jalbert","Leilani Outman","Mara Rudder","Madelyn Mauldin","Sandie Hiers","Iris Daigneault","Paulette Royals","Luvenia Oathout","Shery Easley","Carman Plum","Ida Macey","Pamella Hardie","Luba Benedetto","Leila Oshiro","Kristy Strickler","Leana Greenan","Cordie Ashworth","Bethany Connell","Viviana Schenkel","Tammy Garn","Elida Dieguez","Kiesha Pridgeon","Marge Mcmillion","Jeri Barrs","Ligia Sjogren","Madeline Withers","Philomena Toone","Josefina Birch","Grazyna Smyth","Karey Otte","Alejandra Beauchamp","Nita Schoepp","Alona Gallegos","Dierdre Gerhart","Bee Ozment","Reda Donahoe","Margarette Zinck","Debbie Alverez","Euna Orme","Cristie Lisby","Breana Gorrell","Erlene Mcgibbon","Cleta Kitchin","Anabel Dade","Jacqulyn Suda","Loree Blunt","Earlean Saeed","Felecia Bridgett"]
-  , names         = names_male.concat(names_female)
-  , randListItem = function randListItem(list) {
+var names_male        = ["Andrew Beckel","Mauricio Esterly","Gale Tarry","Winfred Dressel","Clement Monroe","Raymon Symons","Michale Deblois","Irvin Sturgis","Chi Meltzer","Bobby Martucci","Trey Schuessler","Toney Hysell","Tom Croley","David Pia","Lavern Lederman","Carter Lapp","Leopoldo Debelak","Spencer Whiteman","Everette Truax","Daryl Clouse","Jules Strothers","Shannon Siqueiros","Donn Nale","Gerard Stake","Kurt Plascencia","Byron Mignone","Sherman Horner","Scott Musick","Aubrey Doster","Dominique Goncalves","Jess Darr","Ben Bruemmer","Anthony Parks","Hugo Statton","Herman Carson","Adolph Silvas","Reggie Kunzman","Alfonso Dix","Howard Olmo","Earnest Sedlock","Phil Araya","Mervin Oshiro","Kerry Dugger","Wilber Rasberry","Samuel Walthall","Malcolm Mcmillan","Danial Franson","Cliff Lemarr","Sal Schewe","Daniel Sligh"]
+  , names_female      = ["Arie Pates","Ellena Crossett","Annmarie Jalbert","Leilani Outman","Mara Rudder","Madelyn Mauldin","Sandie Hiers","Iris Daigneault","Paulette Royals","Luvenia Oathout","Shery Easley","Carman Plum","Ida Macey","Pamella Hardie","Luba Benedetto","Leila Oshiro","Kristy Strickler","Leana Greenan","Cordie Ashworth","Bethany Connell","Viviana Schenkel","Tammy Garn","Elida Dieguez","Kiesha Pridgeon","Marge Mcmillion","Jeri Barrs","Ligia Sjogren","Madeline Withers","Philomena Toone","Josefina Birch","Grazyna Smyth","Karey Otte","Alejandra Beauchamp","Nita Schoepp","Alona Gallegos","Dierdre Gerhart","Bee Ozment","Reda Donahoe","Margarette Zinck","Debbie Alverez","Euna Orme","Cristie Lisby","Breana Gorrell","Erlene Mcgibbon","Cleta Kitchin","Anabel Dade","Jacqulyn Suda","Loree Blunt","Earlean Saeed","Felecia Bridgett"]
+  , names             = names_male.concat(names_female)
+  , image_categories  = ["abstract","animals","business","cats","city","food","nightlife","fashion","people","nature","sports","technics","transport"]
+  , randListItem      = function randListItem(list) {
     return list[Math.floor((list.length - 1) * Math.random())];
+  }
+  , randomImage = function(id, text, cat) {
+    return "http://lorempixel.com/200/100/" + cat + "/" + (id+1) + "/" + text;
   };
 
 function ExampleController_1(scope) {
@@ -54,16 +58,17 @@ function ExampleController_3(scope) {
     length: 0,
     size: 4
   };
-  scope.data = [
-    {text: 'Image 1'},
-    {text: 'Image 2'},
-    {text: 'Image 3'},
-    {text: 'Image 4'},
-    {text: 'Image 5'},
-    {text: 'Image 6'},
-    {text: 'Image 7'},
-    {text: 'Image 8'}
-  ];
+  scope.data = [];
+  for(var i = 0; i < 10; i++) {
+    var text = "Image " + (i+1)
+      , cat = randListItem(image_categories)
+    ;
+    scope.data.push({
+      text: text,
+      src: randomImage(i, text, cat),
+      cat: cat
+    });
+  }
   scope.pagination.length = scope.data.length - scope.pagination.size + 1;
 }
 
